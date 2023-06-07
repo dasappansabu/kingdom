c9d3d0b15da4b1f848f61ae8971c084fc2ee0808
dasappansabu <135646309+dasappansabu@users.noreply.github.com>
2023-06-07 20:08:20 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,89 +1,90 @@
-#Encoding and Reconstruction of PCM
+#exp 1
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-f=2
+f=2.1
 fs=16
-ts=1/fs
-t=np.arange(0,1,ts)
-x_t=255*(1+np.sin(2*np.pi*f*t))
-#check input
-plt.stem(t,x_t)
-plt.show
+t=np.arange(0,1,1/fs)
+x=(255/2)*(1+np.sin(2*np.pi*f*t))
 
-#quantization
-u_256=[np.round(i) for i in x_t]
-u_128=[np.round(i/2)*2 for i in x_t]
-u_64=[np.round(i/4)*4 for i in x_t]
-u_32=[np.round(i/8)*8 for i in x_t]
-u_16=[np.round(i/16)*16 for i in x_t]
-
-
-plt.plot(u_256)
-plt.plot(u_128)
-plt.plot(u_64)
-plt.plot(u_32)
-plt.plot(u_16)
-
-
-def quantize(x_t,l):
-    step=int(256/l)
-    v=[int(np.round(i/step)*step) for i in x_t]
-    return v
-v=quantize(x_t,16)
-print(v)
-
-def encd(v,l):
-    r=np.log2(l)
-    t=[]
-    for i in v:
-        b=[int(j) for j in np.binary_repr(i)]
-        t=np.append(t,b)
-    return t
-t=encd(v,16)
-plt.stem(t)
-
-
-
-
-
-
-Px=0.5*(255/2)**2
-#Pn=(1/len(x_t))*np.sum(np.square(xq-x_t))
-
-
-SNR=np.zeros(5)
-
-
-xq_256=quantize(x_t,256)
-xq_128=quantize(x_t,128)
-xq_64=quantize(x_t,64)
-xq_32=quantize(x_t,32)
-xq_16=quantize(x_t,16)
-
-
-Pn=[
-    (1/len(x_t))*np.sum(np.square(xq_16-x_t)),
-    (1/len(x_t))*np.sum(np.square(xq_32-x_t)),
-    (1/len(x_t))*np.sum(np.square(xq_64-x_t)),
-    (1/len(x_t))*np.sum(np.square(xq_128-x_t)),
-    (1/len(x_t))*np.sum(np.square(xq_256-x_t))
-]
-
-
-for i in np.arange(5):
-    SNR[i]=10*np.log10(Px/Pn[i])
+def quantize(sample,L):
+    if L==256:
+        return int(round(sample))
+    elif L==128:
+        return int(round(sample/2)*2)
+    elif L==64:
+        return int(round(sample/4)*4)
+    elif L==32:
+        return int(round(sample/8)*8)
+    elif L==16:
+        return int(round(sample/16)*16)
+    elif L==8:
+        return int(round(sample/32)*32)
+    elif L==4:
+        return int(round(sample/64)*64)
+    elif L==2:
+        return int(round(sample/128)*128)
     
-R=np.array([4,5,6,7,8])
+def encode(sample,L):
+    R=int(np.log2(L))
+    return bin(quantize(sample,L))[2:].zfill(R)
+def decode(code,L):
+    R=int(np.log2(L))
+    return quantize(int(code,2),L)
+
+L_values=[256,128,64,32,16,8,4,2]
+quantised_samples={}
+encoded_samples={}
+
+for L in L_values:
+    quantised_samples[L]=np.array([quantize(sample,L) for sample in x])
+    encoded_samples[L]=np.array([encode(sample,L) for sample in x])
+
+
+#bit stream
+encoded_concatenated = np.concatenate([encoded_samples[256]])
+# Convert bit stream to binary string
+bit_stream = ''.join(encoded_concatenated)
+
+plt.stem(range(len(bit_stream)), [int(bit) for bit in bit_stream])
+plt.xlim([0, len(bit_stream)])
+plt.ylim([-0.1, 1.1])
+plt.xlabel('Bit Index')
+plt.ylabel('Bit Value')
+plt.title('Bit Stream')
+plt.show()
 
 
 
+#t1=range(len(bitstream))plt.ylim(-0.1,1.1)plt.stem(t1,bitstream)plt.title('Encoded bit stream')plt.show()
 
-plt.figure(figsize=(12,8))
-plt.plot(R,SNR)
+decoded_samples={}
+reconstructed_signals={}
+for L in L_values:
+    decoded_samples[L]=np.array([decode(code,L) for code in encoded_samples[L]])
+    reconstructed_signals[L]=decoded_samples[L]/(L/255)
+    
+plt.plot(t,x,label="original signal")
+
+plt.plot(t,reconstructed_signals[256],label="reconstructed_signals")
+plt.legend()
+plt.tight_layout()
+
+#snr
+px=0.5*(255/2)**2
+SNR={}
+for L in L_values:
+    pn=np.sum(((quantised_samples[L]-x)**2)/len(x))
+    SNR[L]=10*np.log10(px/pn)
+
+R_values=[int(np.log2(L))for L in L_values] 
+SNR_values=[SNR[L] for L in L_values]
+plt.figure()
+plt.plot(R_values,SNR_values,'-o')
+plt.title('SNR VS no of bits')
 plt.grid()
-plt.xlabel('number of bits of encoder')
-plt.ylabel('SNR')
-plt.title('PCM:SNR vs number of bits')
+plt.show()
+
+
+plt.scatter(reconstructed_signals[256],reconstructed_signals[256],label="reconstructed_signals")
 plt.show()
